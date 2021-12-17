28228e92b5d0826ca3b05816b696564ee607f87f
Patrick Adutwum <adutwum.pa@gmail.com>
2021-12-17 07:55:59 +0000

    set up

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'src/ui/route/weather.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Weather(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/src/ui/route/weather.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Weather extends StatefulWidget {
+  const Weather({Key? key}) : super(key: key);
+
+  @override
+  _WeatherState createState() => _WeatherState();
+}
+
+class _WeatherState extends State<Weather> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather'),
+      ),
+    );
+  }
+}
